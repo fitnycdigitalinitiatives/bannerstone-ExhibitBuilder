53158eb80b890f452466912bf45f83ba0ef96054
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2018-11-01 15:20:21 -0400

    Update title layout

--- views/public/exhibits/show.php
+++ views/public/exhibits/show.php
@@ -3,12 +3,6 @@ echo head(array(
     'title' => metadata('exhibit_page', 'title') . ' &middot; ' . metadata('exhibit', 'title'),
     'bodyclass' => 'exhibits show'));
 ?>
-<nav class="navbar bg-white d-none d-md-flex" id="exhibit-nav">
-  <?php echo exhibit_builder_link_to_exhibit($exhibit, null, array('class' => 'navbar-brand text-dark ml-5')); ?>
-  <button class="navbar-toggler btn" type="button" data-toggle="collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30" focusable="false"><title>Menu</title><path stroke="#f9683b" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" d="M4 7h22M4 15h22M4 23h22"></path></svg>
-  </button>
-</nav>
   <div class="container-fluid">
     <div id="exhibit-blocks">
     <?php exhibit_builder_render_exhibit_page(); ?>
@@ -25,4 +19,7 @@ echo head(array(
       <?php endif; ?>
     </div>
   </div>
-<?php echo foot(array('bodyid'=>'exhibit')); ?>
+  <nav class="navbar fixed-bottom bg-light d-none d-md-flex" id="exhibit-footer">
+    <?php echo exhibit_builder_link_to_exhibit($exhibit, null, array('class' => 'navbar-brand text-dark')); ?>
+  </nav>
+<?php echo foot(); ?>
--- views/public/exhibits/summary.php
+++ views/public/exhibits/summary.php
@@ -1,11 +1,5 @@
 <?php echo head(array('title' => metadata('exhibit', 'title'), 'bodyclass'=>'exhibits summary')); ?>
 
-  <nav class="navbar bg-white d-none d-md-flex" id="exhibit-nav">
-    <?php echo exhibit_builder_link_to_exhibit($exhibit, null, array('class' => 'navbar-brand text-dark ml-5')); ?>
-    <button class="navbar-toggler btn" type="button" data-toggle="collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30" focusable="false"><title>Menu</title><path stroke="#f9683b" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" d="M4 7h22M4 15h22M4 23h22"></path></svg>
-    </button>
-  </nav>
   <div class="container-fluid">
     <div class="row full-screen" id="text-only">
       <div class="col-md-6 col-lg-5 col-xl-4" id="text-only-block">
@@ -29,17 +23,14 @@
           </div>
       </div>
     </div>
-
-    <?php
-    $pageTree = exhibit_builder_page_tree();
-    if ($pageTree):
-    ?>
-    <?php endif; ?>
     <div class="d-md-none" id="exhibit-nav">
       <?php if ($firstPage = $exhibit->getFirstTopPage()): ?>
         <?php echo exhibit_builder_link_to_exhibit($exhibit, '<i class="material-icons">keyboard_arrow_right</i><span class="sr-only">Next</span>', array('role' => 'button', 'class' => 'btn btn-arrow btnNext', 'aria-label' => 'Next'), $firstPage); ?>
       <?php endif; ?>
     </div>
-  </div>
+  </div>  
+  <nav class="navbar fixed-bottom bg-light d-none d-md-flex" id="exhibit-footer">
+    <?php echo exhibit_builder_link_to_exhibit($exhibit, null, array('class' => 'navbar-brand text-dark')); ?>
+  </nav>
 
-<?php echo foot(array('bodyid'=>'exhibit')); ?>
+<?php echo foot(); ?>
